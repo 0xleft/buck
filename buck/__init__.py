e4c2446d1c9ed4ddfaa62a2e322d1459b8e2f4dd
plusleft <107749872+0xleft@users.noreply.github.com>
2024-02-24 18:33:20 +0000

    Update __init__.py

--- buck/__init__.py
+++ buck/__init__.py
@@ -2,32 +2,57 @@ import argparse
 import http.server
 import socketserver
 
-path = ""
+input = ""
 
 class FileHandler(http.server.SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
     def do_GET(self):
-        with open(path, 'rb') as file:
+        with open(input, 'rb') as file:
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
             self.wfile.write(file.read())
 
+class TextHandler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(input.encode())
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('path', type=str)
-    parser.add_argument('port', type=int)
+    parser.add_argument('input', type=str, help='Input file or text')
+    parser.add_argument('mode', type=str, choices=['file', 'text'])
+    parser.add_argument('--port', type=int, default=8000)
     args = parser.parse_args()
 
     # haky
-    global path
-    path = args.path
+    global input
+    input = args.input
 
-    with socketserver.TCPServer(('', args.port), FileHandler) as httpd:
-        print(f'Serving {args.path} at http://localhost:{args.port}')
-        httpd.serve_forever()
+    if args.mode == 'file':
+        handler = FileHandler
+        try:
+            with open(args.input, 'rb') as file:
+                pass
+        except FileNotFoundError:
+            print(f'File {args.input} not found')
+            return
+    else:
+        handler = TextHandler
+
+    with socketserver.TCPServer(('', args.port), handler) as httpd:
+        print(f'Serving at http://localhost:{args.port}')
+        try:
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            httpd.shutdown()
 
 if __name__ == '__main__':
     main()
